ca942b36ad522625fe74c55f35c3388292d6a944
sayeg84 <sayeg_84@hotmail.com>
2026-01-07 16:47:14 +0100

    Make custom installation due to cython extensions

--- setup.py
+++ setup.py
@@ -8,45 +8,41 @@ NAME = "mlcg"
 EXCLUDE_FOLDERS = [
     "opt_radius",
 ]
-# read the version number from the library
-pattern = r"[0-9]\.[0-9]\.[0-9]"
-VERSION = None
-with open("./mlcg/__init__.py", "r") as fp:
-    for line in fp.readlines():
-        if "__version__" in line:
-            VERSION = re.findall(pattern, line)[0]
-if VERSION is None:
-    raise ValueError("Version number not found.")
+
+VERSION = "0.1.2"
 
 
-with open("requirements.txt") as f:
-    install_requires = list(
-        filter(lambda x: "#" not in x, (line.strip() for line in f))
-    )
-
+packages=find_packages(where="src",exclude=EXCLUDE_FOLDERS)
+print(packages)
 extensions = [
     Extension(
         "*",
-        ["mlcg/datasets/cython/*.pyx"],
+        ["src/mlcg/datasets/cython/*.pyx"],
         include_dirs=[np.get_include()],
         define_macros=[("NPY_NO_DEPRECATED_API", "NPY_1_7_API_VERSION")],
     ),
 ]
 
+cython_ext=cythonize(
+        extensions, compiler_directives={"language_level": "3"}
+)
+
+
 setup(
     name=NAME,
     version=VERSION,
-    packages=find_packages(exclude=EXCLUDE_FOLDERS),
+    packages=packages,
     zip_safe=True,
     python_requires=">=3.8",
     license="MIT",
     author="Fe"
     + "\u0301"
     + "lix Musil, Nick Charron, Yoayi Chen, Atharva Kelkar, Clark Templeton",
-    install_requires=install_requires,
-    ext_modules=cythonize(
-        extensions, compiler_directives={"language_level": "3"}
-    ),
+    #install_requires=install_requires,
+    ext_modules=cython_ext,
+)
+
+"""
     scripts=[
         "scripts/mlcg-train.py",
         "scripts/mlcg-nvt_langevin.py",
@@ -55,4 +51,4 @@ setup(
         "scripts/mlcg-train_h5.py",
         "scripts/mlcg-train_h5_ng.py",
     ],
-)
+"""
