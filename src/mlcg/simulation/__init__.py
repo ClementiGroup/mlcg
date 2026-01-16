2d3e9ad82acbf49456f533f748eaa0b55ddd729d
lianctrl <luca.sagresti@gmail.com>
2026-01-16 12:00:54 +0100

    added LangevinCOnstraint

--- src/mlcg/simulation/__init__.py
+++ src/mlcg/simulation/__init__.py
@@ -1,5 +1,6 @@
 from .langevin import LangevinSimulation, OverdampedSimulation
 from .parallel_tempering import PTSimulation
+from .langevin_constraint import LangevinConstraint
 from .base import _Simulation
 from .cli import parse_simulation_config
 from .specialize_prior import condense_all_priors_for_simulation
