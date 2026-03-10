b81ecf6350cfc14eb7735411221b0ecf87df9bab
jacopoventurin <venturinjacopo@gmail.com>
2026-03-10 16:24:40 +0100

    added base cutoff function and derivatives in triton

---
+++ src/mlcg/nn/kernels/models/cutoffs/__init__.py
@@ -0,0 +1,1 @@
+from .cosine_cutoff import _cosine_cutoff, _d_cosine_cutoff_dd, _d2_cosine_cutoff_dd2
---
+++ src/mlcg/nn/kernels/models/cutoffs/cosine_cutoff.py
@@ -0,0 +1,37 @@
+import triton
+import triton.language as tl
+
+triton_pi = tl.constexpr(3.141592653589793)
+
+# ============================================================================
+# Utility funcitons
+# ============================================================================
+@triton.jit
+def _cosine_cutoff(
+        dist,
+        cutoff_upper: tl.constexpr
+):
+    factor = triton_pi / cutoff_upper
+    C = 0.5 * (tl.cos(dist * factor) + 1.0)
+    mask_dist = dist < cutoff_upper
+    return tl.where(mask_dist, C, 0.0)
+    
+@triton.jit
+def _d_cosine_cutoff_dd(
+        dist,
+        cutoff_upper: tl.constexpr
+):
+    factor = triton_pi / cutoff_upper
+    C = -0.5 * tl.sin(dist * factor) * factor
+    mask_dist = dist < cutoff_upper
+    return tl.where(mask_dist, C, 0.0)
+
+@triton.jit
+def _d2_cosine_cutoff_dd2(
+        dist,
+        cutoff_upper: tl.constexpr
+):
+    factor = triton_pi / cutoff_upper
+    C = -0.5 * tl.cos(dist * factor) * factor * factor
+    mask_dist = dist < cutoff_upper
+    return tl.where(mask_dist, C, 0.0)
