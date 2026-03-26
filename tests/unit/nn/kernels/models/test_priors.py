fad676c18c9131a8e4266906cf3bc8b053f72114
jacopoventurin <venturinjacopo@gmail.com>
2026-03-26 17:26:55 +0100

    updated prior kernel test

--- tests/unit/nn/kernels/models/test_priors.py
+++ tests/unit/nn/kernels/models/test_priors.py
@@ -2,24 +2,24 @@ import pytest
 import torch
 
 from mlcg.nn.prior.repulsion import Repulsion, FlashRepulsion
+from mlcg.nn.prior.harmonic import HarmonicBonds, FlashHarmonicBonds
 
 from mlcg.geometry import compute_statistics, fit_baseline_models
 from mlcg.nn import GradientsOut
 
-from mlcg.mol_utils import MolDatabase
+from mlcg.mol_utils import MolDatabase, _ASE_prior_model
 from mlcg.neighbor_list.neighbor_list import atomic_data2neighbor_list
 
 CUTOFF_UPPER = 4.0
 DEVICES = ["cpu"] + (["cuda"] if torch.cuda.is_available() else [])
 
 
-def create_data(prior_name):
-    database = MolDatabase()
-    data = database.collated_data
-
-    if prior_name == Repulsion.name:
+def create_data(prior):
+    if prior.name == Repulsion.name:
+        database = MolDatabase()
+        data = database.collated_data
         data.neighbor_list = {
-            prior_name: atomic_data2neighbor_list(
+            prior.name: atomic_data2neighbor_list(
                 database.collated_data,
                 CUTOFF_UPPER,
                 self_interaction=False,
@@ -27,37 +27,53 @@ def create_data(prior_name):
                 nls_distance_method="torch",
             )
         }
+        stat = compute_statistics(
+            data,
+            prior.name,
+            1.6,
+            prior,
+            bmin=0,
+            bmax=10,
+            fit_from_values=True,
+        )
+
+        base_prior = GradientsOut(prior(stat))
+        flash_prior = GradientsOut(
+            FlashRepulsion(base_prior.model.sigma, prior.name)
+        )
+
+    elif prior.name == HarmonicBonds.name:
+        model_with_data = _ASE_prior_model(sum_out=False)
+        data = model_with_data["collated_prior_data"]
+        base_prior = model_with_data["model"][HarmonicBonds.name]
+        flash_prior = GradientsOut(
+            FlashHarmonicBonds(
+                base_prior.model.k,
+                base_prior.model.x_0,
+                HarmonicBonds.name
+            )
+        )
 
     else:
         raise NotImplementedError(
             f"Test doesn't support prior {prior_name}, please add neighborlist support in create_data function"
         )
 
-    return data
+    return data, base_prior, flash_prior
 
 
 @pytest.mark.parametrize("device", DEVICES)
 @pytest.mark.parametrize(
-    "Base_prior, Flash_prior", [(Repulsion, FlashRepulsion)]
+    "Base_prior, Flash_prior", 
+    [
+        (Repulsion, FlashRepulsion),
+        (HarmonicBonds, FlashHarmonicBonds),
+    ]
 )
 def test_repulsion_kernel(device, Base_prior, Flash_prior):
 
-    data = create_data(Base_prior.name)
+    data, base_prior, flash_prior = create_data(Base_prior)
 
-    stat = compute_statistics(
-        data,
-        Base_prior.name,
-        1.6,
-        Base_prior,
-        bmin=0,
-        bmax=10,
-        fit_from_values=True,
-    )
-
-    base_prior = GradientsOut(Base_prior(stat))
-    flash_prior = GradientsOut(
-        Flash_prior(base_prior.model.sigma, Base_prior.name)
-    )
     compiled_flash_prior = torch.compile(flash_prior)
 
     data = data.to(device)
@@ -66,13 +82,13 @@ def test_repulsion_kernel(device, Base_prior, Flash_prior):
     compiled_flash_prior = compiled_flash_prior.to(device)
 
     data = base_prior(data)
-    base_energy = data.out[Repulsion.name]["energy"]
-    base_forces = data.out[Repulsion.name]["forces"]
+    base_energy = data.out[Base_prior.name]["energy"]
+    base_forces = data.out[Base_prior.name]["forces"]
 
     data.out = {}
     data = flash_prior(data)
-    flash_energy = data.out[Repulsion.name]["energy"]
-    flash_forces = data.out[Repulsion.name]["forces"]
+    flash_energy = data.out[Base_prior.name]["energy"]
+    flash_forces = data.out[Base_prior.name]["forces"]
 
     torch.testing.assert_close(base_energy, flash_energy)
     torch.testing.assert_close(base_forces, flash_forces)
@@ -80,8 +96,8 @@ def test_repulsion_kernel(device, Base_prior, Flash_prior):
     if device == "cuda":
         data.out = {}
         data = compiled_flash_prior(data)
-        compiled_flash_energy = data.out[Repulsion.name]["energy"]
-        compiled_flash_forces = data.out[Repulsion.name]["forces"]
+        compiled_flash_energy = data.out[Base_prior.name]["energy"]
+        compiled_flash_forces = data.out[Base_prior.name]["forces"]
 
         torch.testing.assert_close(base_energy, compiled_flash_energy)
         torch.testing.assert_close(base_forces, compiled_flash_forces)
